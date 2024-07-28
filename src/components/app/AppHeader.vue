8063a94623b16fa7b228bc33c55bc7ae5a7a7692
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-28 14:00:41 +0530

    feat: modify app title.

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -2,6 +2,6 @@
 </script>
 <template>
     <v-app-bar app color="primary">
-        <v-app-bar-title>Reehub</v-app-bar-title>
+        <v-app-bar-title>Secret Sharer</v-app-bar-title>
     </v-app-bar>
 </template>
