82e69ee9d6771907747c5c7e71d2894a8c8d394f
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-28 14:00:15 +0530

    feat: remove logout button from the header.

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,9 +1,7 @@
 <script setup>
-import AppButtonLogout from './AppButtonLogout.vue'
 </script>
 <template>
     <v-app-bar app color="primary">
         <v-app-bar-title>Reehub</v-app-bar-title>
-        <AppButtonLogout />
     </v-app-bar>
 </template>
