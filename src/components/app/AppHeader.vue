27e3ac691d2dd76803c89390bfb5d0ba5b0deea5
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-28 14:20:28 +0530

    feat: modify header props.

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <v-app-bar app color="primary">
+    <v-app-bar density="compact" scroll-threshold="0" scroll-behavior="hide" app color="primary">
         <v-app-bar-title>Secret Sharer</v-app-bar-title>
     </v-app-bar>
 </template>
