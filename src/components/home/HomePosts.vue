a028a10a51f76151ad2c7fdd6d07ec22f6e18bff
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-28 14:13:50 +0530

    feat: add dummy posts.

---
+++ src/components/home/HomePosts.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {usePostStore} from '@/stores'
+
+const postStore = usePostStore()
+</script>
+<template>
+    <v-container>
+        <v-row v-for="post in postStore.posts" :key="post.id">
+            <v-col>
+                <v-card>
+                    <v-card-title class="text-center text-blue-grey font-weight-bold">
+                        {{ post.title }}
+                    </v-card-title>
+                    <v-card-text>
+                        {{ post.body }}
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HomeCreatePost from '@/components/home/HomeCreatePost.vue'
+import HomePosts from '@/components/home/HomePosts.vue'
 </script>
 <template>
   <HomeCreatePost />
+  <HomePosts />
 </template>
