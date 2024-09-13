3aff013524db35a32c60eea4624fbc9f75adb97d
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-09-13 17:11:03 +0530

    feat: add validation.

--- src/components/home/HomeCreatePost.vue
+++ src/components/home/HomeCreatePost.vue
@@ -1,24 +1,33 @@
 <script setup>
 import {ref} from 'vue'
 import { usePostStore, useGeneralStore } from '@/stores'
+import { v4 as uuidv4 } from 'uuid';
 
 const title = ref('')
 const body = ref('')
+const titleInput = ref(null)
+const bodyInput = ref(null)
 const generalStore = useGeneralStore()
 const postStore = usePostStore()
 
-function createNewPost() {
-    const post = {
-        id: postStore.posts.length,
-        title: title.value,
-        body: body.value,
+async function createNewPost() {
+    const isTitleValid = !(Object.keys(await titleInput.value.validate()).length)
+    const isBodyValid = !(Object.keys(await bodyInput.value.validate()).length)
+
+    if (isTitleValid && isBodyValid) {
+        const id = uuidv4()
+        const post = {
+            id: id,
+            title: title.value,
+            body: body.value,
+        }
+        postStore.createNewPost(post)
+        title.value = null
+        body.value = null
+        generalStore.setSnackbarMessage('The post has been created successfully.')
+        generalStore.setSnackbarColor('success')
+        generalStore.showSnackbar()
     }
-    postStore.createNewPost(post)
-    title.value = ''
-    body.value = ''
-    generalStore.setSnackbarMessage('The post has been created successfully.')
-    generalStore.setSnackbarColor('success')
-    generalStore.showSnackbar()
 }
 </script>
 <template>
@@ -29,18 +38,22 @@ function createNewPost() {
                     <v-card-title class="text-center text-primary">Create post</v-card-title>
                     <v-card-text class="mt-6">
                         <v-textarea
+                            ref="titleInput"
                             density="compact"
                             label="Title"
                             v-model="title"
                             rows="1"
                             auto-grow
+                            :rules="[v => !!v || 'Title is required.']"
                         />
                         <v-textarea
+                            ref="bodyInput"
                             density="compact"
                             label="Body"
                             v-model="body"
                             rows="4"
                             auto-grow
+                            :rules="[v => !!v || 'Body is required.']"
                         />
                     </v-card-text>
                     <v-card-actions class="justify-end">
