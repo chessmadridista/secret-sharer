3dec702fe314bb3e399f99dff59c61a75dcb0e7c
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-09-17 19:42:41 +0530

    feat: add button to view more posts.

---
+++ src/components/home/HomeButtonViewMore.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template>
+    <v-container class="text-center">
+        <v-btn color="primary" variant="outlined">
+            View more
+        </v-btn>
+    </v-container>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import HomeCreatePost from '@/components/home/HomeCreatePost.vue'
 import HomePosts from '@/components/home/HomePosts.vue'
+import HomeButtonViewMore from '@/components/home/HomeButtonViewMore.vue';
 </script>
 <template>
   <HomeCreatePost />
   <HomePosts />
+  <HomeButtonViewMore />
 </template>
