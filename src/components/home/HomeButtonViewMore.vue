408667b1dc71826053c6396ad70554aecbc4c4a0
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-09-19 19:00:28 +0530

    feat: add baseUrl.

--- src/components/home/HomeButtonViewMore.vue
+++ src/components/home/HomeButtonViewMore.vue
@@ -8,6 +8,7 @@ function viewMore() {
     postStore.increasePostFetchIndices()
     const startIndex = postStore.postStartIndex
     const endIndex = postStore.postEndIndex
+    let baseUrl
     if (process.env.NODE_ENV === 'development') {
         baseUrl = 'http://127.0.0.1:5000'
     } else if (process.env.NODE_ENV === 'production') {
